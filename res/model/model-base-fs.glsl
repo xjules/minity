e3117a15569490259b892481e477ba4e71c91ceb
Julius Parulek <parulek@gmail.com>
2023-10-01 23:00:11 +0200

    Add phong shading model

--- res/model/model-base-fs.glsl
+++ res/model/model-base-fs.glsl
@@ -28,6 +28,30 @@ void main()
 		float smallestDistance = min(min(fragment.edgeDistance[0],fragment.edgeDistance[1]),fragment.edgeDistance[2]);
 		float edgeIntensity = exp2(-1.0*smallestDistance*smallestDistance);
 		result.rgb = mix(result.rgb,wireframeLineColor.rgb,edgeIntensity*wireframeLineColor.a);
+	} else {
+		// Normalize vectors
+		vec3 N = normalize(fragment.normal);
+		vec3 L = normalize(worldCameraPosition - fragment.position);
+
+		// Compute ambient
+		vec3 lightColor = vec3(1.0,1.0,1.0);
+		// vec3 ambient = materialAmbient * lightColor;
+		vec3 ambient = vec3(0.0,0.0,0.0);
+
+		// Compute diffuse
+		float diff = max(dot(N, L), 0.0);
+		vec3 diffuse = diffuseColor * (diff * lightColor);
+
+		// Compute specular
+		float materialShininess = 128.0;
+		float materialSpecular = 1.0f;
+		vec3 V = normalize(-fragment.position);
+		vec3 R = reflect(-L, N);  
+		float spec = pow(max(dot(V, R), 0.0), materialShininess);
+		vec3 specular = materialSpecular * (spec * lightColor); 
+
+		// Combine all components
+		result.rgb = ambient + diffuse + specular;
 	}
 
 	fragColor = result;
